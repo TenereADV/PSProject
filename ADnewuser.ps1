1aea99b23642ce61d6d0f1ad21077f8b9306edf8
TenereADV <90269255+TenereADV@users.noreply.github.com>
2021-09-07 14:04:04 -0700

    Add files via upload

--- ADnewuser.ps1
+++ ADnewuser.ps1
@@ -1,4 +1,5 @@
-﻿#Prompts the user for the new user's information to populate AD fields.
+﻿##Prompts the user for the new employee's information to populate AD fields.##
+
 
 #Function used to allow assignment of a department.
 Function Dept-Menu
@@ -14,15 +15,16 @@ Function Dept-Menu
 }
 
 
-$First = Read-Host "Enter the employees's first name"
+$First = Read-Host "Enter the employee's first name"
 $Last = Read-Host "Enter the employees's last name"
 $Name = $First + " " + $Last
 $Dname = $Name
 $SamName = $First
-$PW = Read-Host -AsSecureString "Enter a temporary password. User will change it on their first login"
 
-#Switch used to assign a department
+#Switch used to assign a department. 
+Do{
 Dept-Menu
+$repeat = $false 
     $Selection = Read-Host "Choose the employee's department"
         Write-Host ""
 
@@ -33,7 +35,10 @@ Dept-Menu
         '3'{'Employee will be assigned to the Managers department.'}
         '4'{'Employee will be assigned to the Research department.'}
         '5'{'Employee will be assigned to the Sales department.'}
+        Default {"Please enter a valid selection";$repeat = $true}
     }
+}
+While ($repeat -eq $true)
 
 #If statement to change the user's selection to a department
 If($Selection -eq "1"){
@@ -47,13 +52,36 @@ Elseif ($selection -eq "4"){
 Elseif ($selection -eq "5"){
     $Dept = "Sales"}
 
+#Assigns department to the employee's path
 $Path = "ou=$Dept,dc=adatum,dc=com"
 
-new-aduser -GivenName $First -Surname $Last -name $name -DisplayName $Dname -SamAccountName $SamName -Path $Path -Department $Dept
+#Inserts blank line for formatting.
+Write-Host ""
+
+#Asks user to enable account. Checks for invalid choice.
+Do{ 
+$Enabled = Read-Host "Would you like to enable this account now?"
+If($Enabled -eq "y" -or $enabled -eq "yes"){
+    $Enabled = 1}
+ElseIf($Enabled -eq "n" -or $Enabled -eq "no"){
+    $Enabled = 0}
+Else{
+    Write-Host "Please enter a valid selection."}
+} Until ($enabled -eq 0 -or $enabled -eq 1)
+
+#Converts temp password to secure
+$PW = ConvertTo-SecureString "Pa55w.rd" -AsPlainText -Force
+
+#Creates the new ADUser
+new-aduser -GivenName $First -Surname $Last -name $name -DisplayName $Dname -SamAccountName $SamName -Path $Path -Department $Dept -Enabled $Enabled -AccountPassword $PW -ChangePasswordAtLogon $true
+
+Write-Host "";"Temporary password is 'Pa55w.rd'.  The employee will be forced to change it upon first login."
 
 
-get-aduser -filter "name -like '*test*'" -Properties *
-#Remove-ADUser -Identity test
+Get-ADUser -Identity $SamName -Properties department
+
+#get-aduser -filter "name -like '*test*'" -Properties *
+Remove-ADUser -Identity test
 
 
 
